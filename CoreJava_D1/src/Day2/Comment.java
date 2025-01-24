d641cfaf2a010589942e2754976da87fffc70c3d
Rocktim <rocktimcseru50@gmail.com>
2025-01-24 14:46:06 +0600

    first commit

---
+++ CoreJava_D1/src/Day2/Comment.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		multiple line comment: cntl+shift+/
+		
+		/*
+		 * System.out.println("Single line comment");
+		 * System.out.println("Multiple line comment");
+		 */
+	}
+
+}
