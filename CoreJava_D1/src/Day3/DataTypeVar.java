3759a47feb7fbb5c7fa86cec7a78b53f4079fc82
Rocktim <rocktimcseru50@gmail.com>
2025-01-24 17:12:09 +0600

    Updated files

---
+++ CoreJava_D1/src/Day3/DataTypeVar.java
@@ -0,0 +1,23 @@
+package Day3;
+
+public class DataTypeVar {
+
+	public static void main(String[] args) {
+	// Var data type use in Java 10 to up
+		// it's  can use assign only. not to declare & assign 
+		
+		var b=10;
+		System.out.println(b);
+		var d= 9568867.6778;
+		System.out.println(d);
+		
+		// can't declare and assing. it know only right side
+		
+		var n; //show error
+		n=888;
+		System.out.println(n);
+		
+
+	}
+
+}
---
+++ CoreJava_D1/src/Day3/DataTypes.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		System.out.println(a);
+		float b=356.56f;
+		System.out.println(b);
+		double d = 56867.98679d;
+		System.out.println(d);
+		long l=87569567896797l;
+		System.out.println(l);
+
+	}
+
+}
